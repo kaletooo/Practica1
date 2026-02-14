7cd22b830b4f653afb1a046d5a0c90caaae84f5a
Kaleto Knight <nainkaleto@gmail.com>
2026-02-14 11:43:54 -0600

    Primer Commit con los códigos fuente sin compilar

---
+++ src/ito/poo/App.java
@@ -0,0 +1,15 @@
+package ito.poo;
+
+public class App {
+
+    static Mascota mascota;
+
+    static void run(){
+          mascota= new Mascota("Argos",4,"perro","Beagle");
+          System.out.println(mascota);
+          mascota.cumpleAños();
+          System.out.println("Despues de cumplir años"+mascota);
+    }
+
+    public static void main(String[] args){ run(); }
+}
